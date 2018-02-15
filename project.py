ee08a310b7eca714d917819d4e3e3f6af1066fdc
Khai Le <khai.le@mutualmobile.com>
2018-02-14 21:25:54 -0600

    reset project.py and projectBase.py to default state with examples and updated common and southwire-shared commits.

--- project.py
+++ project.py
@@ -16,6 +16,29 @@ def main():
             'noReset': False,
             'automationName': 'XCUITest'
         })
+    # driver = webdriver.Remote(
+    #     command_executor='http://127.0.0.1:4723/wd/hub',
+    #     desired_capabilities={
+    #         'platformName': 'iOS',
+    #         'platformVersion': '11.0',
+    #         'deviceName': 'iPad Pro (9.7-inch)',
+    #         'browserName': 'Safari',
+    #         'udid': '82DDA8DC-3D06-467B-8537-D3939672B050',
+    #         'newCommandTimeout': 7200,
+    #         'fullReset': True,
+    #         'noReset': False,
+    #         'automationName': 'XCUITest'
+    #     })
+    # driver = webdriver.Remote(
+    #     command_executor='http://127.0.0.1:4723/wd/hub',
+    #     desired_capabilities={
+    #         'platformName': 'Android',
+    #         'platformVersion': '6.0.1',
+    #         'deviceName': 'Nexus 7',
+    #         'browserName': 'Chrome',
+    #         'chromedriverExecutable': '/Users/khaile/Develop/appiumTest/node_modules/appium-chromedriver/chromedriver/mac/chromedriver',
+    #         'newCommandTimeout': 7200
+    #     })
     global app
     global UIType
     app = Device(driver)
--- projectBase.py
+++ projectBase.py
@@ -18,12 +18,39 @@ class ProjectBase(TestCase):
                 'noReset': False,
                 'automationName': 'XCUITest'
             })
+        # self.driver = webdriver.Remote(
+        #     command_executor='http://127.0.0.1:4723/wd/hub',
+        #     desired_capabilities={
+        #         'platformName': 'iOS',
+        #         'platformVersion': '11.2',
+        #         'deviceName': 'iPad Pro (9.7-inch)',
+        #         'browserName': 'Safari',
+        #         'udid': '82DDA8DC-3D06-467B-8537-D3939672B050',
+        #         'newCommandTimeout': 7200,
+        #         'fullReset': True,
+        #         'noReset': False,
+        #         'automationName': 'XCUITest'
+        #     })
+        # self.driver = webdriver.Remote(
+        #     command_executor='http://127.0.0.1:4723/wd/hub',
+        #     desired_capabilities={
+        #         'platformName': 'Android',
+        #         'platformVersion': '6.0.1',
+        #         'deviceName': 'Nexus 7',
+        #         'browserName': 'Chrome',
+        #         'chromedriverExecutable': '/Users/khaile/Develop/appiumTest/node_modules/appium-chromedriver/chromedriver/mac/chromedriver',
+        #         'newCommandTimeout': 7200
+        #     })
         self.driver.implicitly_wait(5)
         self.app = Device(self.driver)
         self.UIType = Type(self.driver)
         self.verification = Verify()
         self.assertion = Assert()
+        self.app.switchToWebview()
         self.isMobile = self.app.isMobile()
+        self.isChromium = False
+        if self.driver.current_context == 'CHROMIUM':
+            self.isChromium = True
 
     def tearDown(self):
         self.driver.quit()
